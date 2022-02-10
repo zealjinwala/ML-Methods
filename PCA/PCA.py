23859263ab0fffd649a2c66729c32ff9ef761290
Zeal Jinwala <zsj24@drexel.edu>
2022-02-10 16:28:40 -0500

    updates

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -8,22 +8,41 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
-filePath = "/Users/zsj24/GitHub/ML-Methods/diseases_subset.csv"
-df = pd.read_csv(filePath,header=None)
-# print(df.head())
-dim = df.shape
-rows = dim[0]
-cols = dim[1]
-data = df.iloc[1:rows,1:cols]
-# print(data.head(5))
-features = df.iloc[0,1:cols]
-# print(features)
+def extractData(filePath):
+    df = pd.read_csv(filePath,header=None)
+    print(df.head())
+    dim = df.shape
+    rows = dim[0]
+    cols = dim[1]
+    data = df.iloc[1:rows,1:cols]
+    features = df.iloc[0,1:cols]
+    return data, features 
+
+def PCAfunc(data): 
+    covarMatrix = PCA(n_components=6)# we have 5 features
+    covarMatrix.fit(data)
+    var = np.cumsum(np.round(covarMatrix.explained_variance_ratio_,decimals=3)*100) #calculate variance ratios
+    plt.ylabel('% Variance Explained')
+    plt.xlabel('# of Features')
+    plt.title('PCA Analysis')
+    plt.ylim(30,100.5)
+    plt.style.context('seaborn-whitegrid')
+    plt.plot(var)
+    return var
+
+# def plotbygroup: 
 
 
 
+# see how much variance is captured with 2 components
+# how many components will gcapture 75% variance? Ans: x components
+# what is the reconstruction error with X components
 
-
+if __name__ == "__main__":
+    filePath = "/Users/zsj24/GitHub/ML-Methods/PCA/diseases_subset.csv"
+    [data, features] = extractData(filePath)
+    tvalues = PCAfunc(data)
---
+++ PCA/learn.py
@@ -0,0 +1,5 @@
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+
+print(cancer.data)
+
